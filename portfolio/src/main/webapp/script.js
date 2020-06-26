f962062002ee06fbd227275a118dccf779678e04
RicardoChapaRomero <ricardochaparomero@gmail.com>
2020-06-26 21:17:19 +0000

    Implement the random image cycler and add facebook link to the page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -51,18 +51,25 @@ const GALLERY_ITEMS = [
   }
 ];
 
-const TIME_INTERVAL = 10000;
-let galleryItemsStack = [];
-let randomImageEvent = false;
+const TIME_INTERVAL = 10000; // Constant time of 10 seconds
+let galleryItemsStack = []; // Stack to keep track of displayed images
+let randomImageEvent = false; // Bool to track onClick events
+let intervalState = null; // Variable to store the setInterval() status
 
 function getRandomImage() {
 
-  let intervalState; 
   randomImageEvent = !randomImageEvent
+  const randomButtonContainer = document.querySelector('.random_picture_button');
 
-  if(!randomImageEvent) {
-    intervlState = setInterval(setRandomImage,TIME_INTERVAL);
+  if(randomImageEvent) {
+
+    //Start the random image cycler
+    intervalState= setInterval(setRandomImage,TIME_INTERVAL);
+    randomButtonContainer.textContent = "Click to stop the random image cycler";
   } else {
+
+    //Stop the random image cycler
+    randomButtonContainer.textContent = "Click to start the random image cycler";
     galleryItemsStack = [];
     clearInterval(intervalState);
     intervalState = null;
@@ -70,14 +77,18 @@ function getRandomImage() {
 }
 
 function setRandomImage() {
-  console.log(galleryItemsStack.length);
+
+  // Restart the stack if all images have been displayed in a cycle
+  if(galleryItemsStack.length === GALLERY_ITEMS.length) galleryItemsStack = [];
 
   // Pick a random gallery item
   let singleGalleryItem = GALLERY_ITEMS[Math.floor(Math.random() * GALLERY_ITEMS.length)];
 
+  // Look for another image if the one has already been displayed in the cycle
   while(galleryItemsStack.indexOf(singleGalleryItem) > -1) {
     singleGalleryItem = GALLERY_ITEMS[Math.floor(Math.random() * GALLERY_ITEMS.length)];
   }
+
   galleryItemsStack.push(singleGalleryItem);
 
   // Insert image to the page
@@ -89,3 +100,6 @@ function setRandomImage() {
   const descriptionContainer = document.getElementById('picture_description_container');
   descriptionContainer.innerText = singleGalleryItem.description;
 }
+
+// When the page starts, display a random image.
+window.onload = setRandomImage;
