de46821084528567e9377e0b99c77bccf031d07c
RicardoChapaRomero <ricardochaparomero@gmail.com>
2020-07-15 23:14:38 +0000

    Make half testing problems

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,61 @@
 
 package com.google.sps;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<TimeRange> possibleMeetingTimes = new ArrayList<TimeRange>(); //Arrays.asList();
+    System.out.println(request.getDuration());
+
+    if(TimeRange.WHOLE_DAY.duration() < request.getDuration()) {
+      return possibleMeetingTimes;
+    }
+    
+    if(events.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+
+    Collection<String> meetingAttendees = request.getAttendees();
+    List<TimeRange> eventTimes = new ArrayList<TimeRange>();
+
+    for(Iterator<Event> iterator = events.iterator(); iterator.hasNext();) {
+      Event event = iterator.next();
+      Set<String> eventAttendees = event.getAttendees();
+
+      Iterator<String> meetingAttendeesList = meetingAttendees.iterator();
+
+      while(meetingAttendeesList.hasNext()) {
+        if (eventAttendees.contains(meetingAttendeesList.next())) {
+          eventTimes.add(event.getWhen());
+          break;
+        }
+      }
+    }
+
+    if (eventTimes.size() == 1) {
+      TimeRange eventTime = eventTimes.get(0);
+      if(eventTime.start() > TimeRange.START_OF_DAY && ((eventTime.start() - TimeRange.START_OF_DAY) >= request.getDuration())) {
+        TimeRange availableBeforeMeeting = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, eventTime.start(), false);
+        possibleMeetingTimes.add(availableBeforeMeeting);
+      }
+      if(eventTime.end() < TimeRange.END_OF_DAY && ((TimeRange.END_OF_DAY - eventTime.end()) >= request.getDuration())) {
+        TimeRange availableAfterMeeting = TimeRange.fromStartEnd(eventTime.end(), TimeRange.END_OF_DAY, true);
+        possibleMeetingTimes.add(availableAfterMeeting);
+      }
+      return possibleMeetingTimes;
+    }
+
+
+    return possibleMeetingTimes;
+    
+    //throw new UnsupportedOperationException("TODO: Implement this method.");
   }
 }
